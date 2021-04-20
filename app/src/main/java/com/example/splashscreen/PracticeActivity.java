c6cec83e051312f3693ed8777c248e27d77a04d7
thomasbuo <thomasbuo@gmail.com>
2021-04-21 01:32:32 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 90586909aae09717095c8a6949df76da4a7dfe5c.

--- app/src/main/java/com/example/splashscreen/PracticeActivity.java
+++ app/src/main/java/com/example/splashscreen/PracticeActivity.java
@@ -142,20 +142,28 @@ public class PracticeActivity extends LinkingFunctions {
             }
 
             System.out.println(counter);
+            // TODO fill in real number of trials for mail sending
             if(counter==2){ //21){
                 System.out.println(context.getFileStreamPath(filename));
                 sendMail.sendMail(context.getFileStreamPath(filename).toString(), filename);
+
+                Intent i = new Intent(this,PracticeDone.class);
+                startActivity(i);
+            }
+            else {
+                Handler handlerTransition = new Handler();
+                handlerTransition.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(PracticeActivity.this, LoadBetweenTestAndPrime.class);
+                        startActivity(i);
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    }
+                }, 1000);
             }
 
-            Handler handlerTransition = new Handler();
-            handlerTransition.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent i = new Intent(PracticeActivity.this, LoadBetweenTestAndPrime.class);
-                    startActivity(i);
-                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                }
-            }, 1000);
+
+
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
