414798e8350c9388353f1f134f0f0612e4ca3834
Lizzy van Sterkenburg <lizzysterkenburg@gmail.com>
2021-04-24 23:18:29 +0200

    Possible fix for dynamic screens (only applied to main screen)

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends LinkingFunctions {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_tutorial_dynamic);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().hide();
 
