15870e2cbaf0519265435df2c53411c5c7c69008
thomasbuo <thomasbuo@gmail.com>
2021-04-28 14:01:07 +0200

    scrollable

--- app/src/main/java/com/example/splashscreen/SplashActivity.java
+++ app/src/main/java/com/example/splashscreen/SplashActivity.java
@@ -1,10 +1,22 @@
 package com.example.splashscreen;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.util.Calendar;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -18,6 +30,4 @@ public class SplashActivity extends AppCompatActivity {
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
         }, 2500);
     }
-
-
 }
