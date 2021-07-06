c4074e0518b8c58dae44e3028f0fb5082b362dba
Sterkenburg, L.M. van (Lizzy) <lizzy.vansterkenburg@student.ru.nl>
2021-07-06 12:26:17 +0200

    edited analsyer to fit new input

--- ResultAnalyser.py
+++ ResultAnalyser.py
@@ -45,14 +45,16 @@ print("\n path = ", path + "\n filename = " + filename + "\n")
 
 input_text = open(os.path.join(path, filename), "r")
 
-data = ['Date',	'Time',	'Sentence',	'Voice']
+data = ['User', 'Session', 'Verb', 'Date',	'Time',	'Sentence',	'Voice']
 
 df = pd.DataFrame(columns=data)
+print(input_text, "inpuuuuuut")
 for i in input_text:
+    print("IM I", i)
     sentence_array = i.split(";")
     sentence = sentence_array[2]
     voiceResult = voice(sentence)
-    data = {"Date": str(sentence_array[1]), "Time": str(sentence_array[0]), "Sentence": sentence_array[2].strip("\n"), "Voice": voiceResult}
+    data = {"Date": str(sentence_array[1]), "Time": str(sentence_array[0]), "Sentence": sentence_array[2].strip("\n"), "User": str(sentence_array[3]), "Session": str(sentence_array[4]), "Verb": str(sentence_array[5]),   "Voice": voiceResult}
     df = df.append(data, ignore_index=True)
 print(df)
 
